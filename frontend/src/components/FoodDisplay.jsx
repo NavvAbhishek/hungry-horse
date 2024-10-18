49e2c0f751abb4d7a409782c6db9b4ca5b2ed311
NavvAbhishek <abi2000navi@gmail.com>
2024-10-18 15:46:20 +0530

    add filter method to food display

--- frontend/src/components/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay.jsx
@@ -1,29 +1,38 @@
 import { useContext } from "react";
 import { StoreContext } from "../context/StoreContext";
 import FoodItem from "./FoodItem";
+import PropTypes from "prop-types";
 
-const FoodDisplay = () => {
+const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
 
   return (
     <div className="mx-28">
-      <h2 className="text-4xl font-bold capitalize mt-6">Top dishes near you</h2>
+      <h2 className="text-4xl font-bold capitalize mt-6">
+        Top dishes near you
+      </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 py-8">
         {food_list.map((item, index) => {
-          return (
-            <FoodItem
-              key={index}
-              id={item._id}
-              name={item.name}
-              description={item.description}
-              price={item.price}
-              image={item.image}
-            />
-          );
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            );
+          }
         })}
       </div>
     </div>
   );
+};
+
+FoodDisplay.propTypes = {
+  category: PropTypes.string.isRequired,
 };
 
 export default FoodDisplay;
