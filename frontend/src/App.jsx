81d669482c36d0ebaa8cac2ea021aa0901b5fd05
NavvAbhishek <abi2000navi@gmail.com>
2024-10-18 16:02:32 +0530

    add footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,24 @@
-import Navbar from "./components/Navbar"
-import { Route, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import PlaceOrder from "./pages/PlaceOrder";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/order" element={<PlaceOrder/>} />
-      </Routes>
-    </div>
-  )
-}
+    <>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
