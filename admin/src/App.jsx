2957b266aab47481f7e61dc48127f48ea94f3a51
NavvAbhishek <abi2000navi@gmail.com>
2024-10-24 22:06:50 +0530

    add admin

---
+++ admin/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar"
+import Sidebar from "./components/Sidebar"
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <hr />
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default App
