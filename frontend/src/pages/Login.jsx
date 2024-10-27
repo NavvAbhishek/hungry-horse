e50b567b57646e0436978a24faf774d9afc23259
NavvAbhishek <abi2000navi@gmail.com>
2024-10-27 16:10:43 +0530

    update login ui

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,45 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { StoreContext } from "../context/StoreContext";
+import axios from "axios";
 
 const Login = () => {
+  const { url, setToken } = useContext(StoreContext);
+
   const [currentState, setCurrentState] = useState("Login");
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onChnageHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  const onLogin = async (e) => {
+    e.preventDefault();
+    let newUrl = url;
+    if (currentState === "Login") {
+      newUrl += "/api/user/login";
+    } else {
+      newUrl += "/api/user/register";
+    }
+    const res = await axios.post(newUrl, data);
+
+    if (res.data.success) {
+      setToken(res.data.token);
+      localStorage.setItem("token", res.data.token);
+    } else {
+      alert(res.data.message);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center">
-      <form action="" className="text-darkBlue">
+      <form onSubmit={onLogin} className="text-darkBlue">
         <h2 className="mt-20 text-center text-4xl font-bold">{currentState}</h2>
         <div className="mt-6 flex flex-col gap-5 ">
           {currentState === "Login" ? (
@@ -12,6 +47,9 @@ const Login = () => {
           ) : (
             <input
               type="text"
+              name="name"
+              value={data.name}
+              onChange={onChnageHandler}
               placeholder="Your name"
               className="border-2 px-4 py-2 border-darkBlue rounded-lg"
               required
@@ -19,12 +57,18 @@ const Login = () => {
           )}
           <input
             type="email"
+            name="email"
+            value={data.email}
+            onChange={onChnageHandler}
             placeholder="Your email"
             className="border-2 px-4 py-2 border-darkBlue rounded-lg"
             required
           />
           <input
             type="password"
+            name="password"
+            value={data.password}
+            onChange={onChnageHandler}
             placeholder="Enter password"
             className="border-2 px-4 py-2 border-darkBlue rounded-lg"
             required
@@ -32,34 +76,39 @@ const Login = () => {
         </div>
         <div className="text-center">
           {currentState === "Register" ? (
-            <button className="blueBtn px-2 py-2 mt-5"> Create Account </button>
+            <button type="submit" className="blueBtn px-2 py-2 mt-5">
+              {" "}
+              Create Account{" "}
+            </button>
           ) : (
-            <button className="blueBtn px-3 py-[5px] mt-5">Login </button>
+            <button type="submit" className="blueBtn px-3 py-[5px] mt-5">
+              Login{" "}
+            </button>
           )}
         </div>
-       <div className="mt-5">
-       {currentState === "Register" ? (
-          <p>
-            Already have an account?{" "}
-            <span
-              onClick={() => setCurrentState("Login")}
-              className="text-darkRed font-semibold cursor-pointer"
-            >
-              Login
-            </span>
-          </p>
-        ) : (
-          <p>
-            Don&apos;t have an account?{" "}
-            <span
-              onClick={() => setCurrentState("Register")}
-              className="text-darkRed font-semibold cursor-pointer"
-            >
-              Register
-            </span>
-          </p>
-        )}
-       </div>
+        <div className="mt-5">
+          {currentState === "Register" ? (
+            <p>
+              Already have an account?{" "}
+              <span
+                onClick={() => setCurrentState("Login")}
+                className="text-darkRed font-semibold cursor-pointer"
+              >
+                Login
+              </span>
+            </p>
+          ) : (
+            <p>
+              Don&apos;t have an account?{" "}
+              <span
+                onClick={() => setCurrentState("Register")}
+                className="text-darkRed font-semibold cursor-pointer"
+              >
+                Register
+              </span>
+            </p>
+          )}
+        </div>
       </form>
     </div>
   );
