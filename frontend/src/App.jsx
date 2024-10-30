cf07dc90f004044220a53a3fb25217be1a202bfb
NavvAbhishek <abi2000navi@gmail.com>
2024-10-31 01:08:20 +0530

    add verify page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Cart from "./pages/Cart";
 import PlaceOrder from "./pages/PlaceOrder";
 import Footer from "./components/Footer";
 import Login from "./pages/Login";
+import Verify from "./pages/Verify";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/verify" element={<Verify/>} />
         </Routes>
       </div>
       <Footer />
