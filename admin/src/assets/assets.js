124509da6100428c28cfb23369aa3e410b30defb
NavvAbhishek <abi2000navi@gmail.com>
2024-11-01 19:26:53 +0530

    update admin order page

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,4 +1,5 @@
 import logo from "./logo.png";
 import profile from "./profile.png"
+import orderImg from "./orderImg.png"
 
-export const assets = { logo, profile };
+export const assets = { logo, profile,orderImg };
