86a377574a49a2fdfb3c8c23389c3351e185b852
NavvAbhishek <abi2000navi@gmail.com>
2024-11-01 19:27:37 +0530

    update admin nav

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { assets } from "../assets/assets";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center mx-4 sm:mx-16 md:mx-28">
-      <img src={assets.logo} width={90} className="cursor-pointer0" alt="" />
-      <img src={assets.profile} width={70} className="cursor-pointer0" alt="" />
+      <img src={assets.logo} width={90} className="cursor-pointer" alt="" />
+      <img src={assets.profile} width={50} className="cursor-pointer" alt="" />
     </div>
   );
 };
